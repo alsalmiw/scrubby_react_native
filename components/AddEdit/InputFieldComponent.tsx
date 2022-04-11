ef642d7dd98a81cac23b83b675490ddfca7e0fee
pVang8 <pvang@codestack.co>
2022-04-11 15:04:08 -0700

    testing icon with input

--- components/AddEdit/InputFieldComponent.tsx
+++ components/AddEdit/InputFieldComponent.tsx
@@ -1,21 +1,44 @@
 import {FC} from 'react'
-import { StyleSheet, TextInput, View } from 'react-native'
+import { Image, StyleSheet, TextInput, View } from 'react-native'
 
-
+import loginIcon from '.../image/Iconfether.png'
 
 const InputFieldComponent: FC =() =>{
     return(
-        <View>
-            <TextInput style={styles.inputBox} onChangeText={(e) => console.log(e)} />
+        <View style={styles.searchSection}>
+            <Image source={loginIcon} style={styles.iconPad} />
+            <TextInput style={styles.inputBox} onChangeText={(e) => console.log(e)} placeholder='username' />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     inputBox:{
-        borderRadius:10,
-        color:'black'
-    }
+        borderRadius:15,
+        borderColor:'black',
+        borderWidth:1,
+        width:300,
+        height:50,
+        textAlign:'center',
+        fontSize:25,
+        flex: 1,
+        paddingTop: 10,
+        paddingRight: 10,
+        paddingBottom: 10,
+        paddingLeft: 0
+    },
+    iconPad:{
+        padding: 10
+    },
+    searchSection: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+
+
 })
 
 export default InputFieldComponent

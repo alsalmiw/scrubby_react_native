e338cc497ad9cb0a0bed0d6adbc20d4130341f60
Daniel Bautista <dbautista9015@gmail.com>
2022-04-12 13:49:54 -0700

    Added plus icon

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
 import ButtonModalComponent from './components/Modal/ButtonModalComponent';
+import AddItemButtonComponent from './components/AddItemButtonComponent';
+import { AntDesign } from '@expo/vector-icons';
 // import { ThemeProvider } from './context/ThemeContext';
 // import HeaderComponent from './components/HeaderComponent'
 
@@ -11,14 +14,25 @@ export default function App() {
     console.log('Hello World')
   }
 
+  const displayAddHandler = () => {
+    console.log('This is the add button');
+  }
+
   return (
     // <ThemeProvider>
     // <HeaderComponent title="Header Component"/>
     <>
       <View style={styles.container}>
         <Text>WhatWhat</Text>
+        {/* This is the ButtonModalComponent */}
         <ButtonModalComponent onPress={displayHandler} >Completed</ButtonModalComponent>
+        {/* This is the AddItemButtonComponent */}
+        <AddItemButtonComponent onPress={displayAddHandler}>
+          {/* This is the expo vector icon, size is based on pixels */}
+          <AntDesign name="plussquare" size={130} color="grey" />
+        </AddItemButtonComponent>
       </View>
+
     </>
     // {/* </ThemeProvider> */}
   );
@@ -31,4 +45,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  addButtonContainer: {
+    flex: 1,
+  }
 });

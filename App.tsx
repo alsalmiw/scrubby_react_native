0c298e1bd175aae215dee037379616020aa26712
Daniel Bautista <dbautista9015@gmail.com>
2022-04-12 10:41:27 -0700

    Tested button in app.tsx and pass a function

--- App.tsx
+++ App.tsx
@@ -1,18 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ButtonModalComponent from './components/Modal/ButtonModalComponent';
 // import { ThemeProvider } from './context/ThemeContext';
 // import HeaderComponent from './components/HeaderComponent'
 
 
 export default function App() {
+
+  const displayHandler = () => {
+    console.log('Hello World')
+  }
+
   return (
     // <ThemeProvider>
     // <HeaderComponent title="Header Component"/>
     <>
-    <View style={styles.container}>
-    <Text>WhatWhat</Text>
- 
-    </View>
+      <View style={styles.container}>
+        <Text>WhatWhat</Text>
+        <ButtonModalComponent onPress={displayHandler} >Completed</ButtonModalComponent>
+      </View>
     </>
     // {/* </ThemeProvider> */}
   );

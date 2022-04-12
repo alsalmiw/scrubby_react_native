458557d09f407acb0e55ae4bd55e34b91cefdd34
alsalmiw <alsalmiw@gmail.com>
2022-04-12 16:52:10 -0700

    added some user data into useContext - Walaa

--- hooks/use-user.js
+++ hooks/use-user.js
@@ -2,5 +2,39 @@ import {useState} from "react"
 
 export default function UseUser(){
 
-    return{}
+    const [username, setUsername] = useState('')
+    const [userData, setUserData] = useState({
+        id: number,
+        username: string,
+        fullname: string,
+        photo: string,
+        points: number, 
+        coins: number,
+        isDeleted: boolean,
+    })
+    const [childData, setChildData] = useState({
+        id: number,
+        userId: number,
+        dependantName: string,
+        dependantAge: number,
+        dependantPhoto: string,
+        dependantCoins: number,
+        dependantPoints: number, 
+        isDeleted: boolean,
+    })
+    const [mySpaces, setMySpaces] =useState({
+        id: number,
+        collectionName: string,
+        isDeleted: boolean,
+        userId: number,
+    })
+
+    const [myRooms, setMyRooms] = useState({
+        id: number,
+        spaceNamne: string,
+        spaceCategory: string,
+        collectionId: number,
+    })
+
+    return{username, setUsername, userData, setUserData, childData, setChildData, mySpaces, setMySpaces, myRooms, setMyRooms}
 }

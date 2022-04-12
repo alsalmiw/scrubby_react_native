b562385f97c2f6425697dbbe0a787675bfb548e7
Daniel Bautista <dbautista9015@gmail.com>
2022-04-12 10:40:56 -0700

    Created Button Component For Modal

---
+++ components/Modal/ButtonModalComponent.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+import { View, Pressable, Text, StyleSheet } from 'react-native';
+
+
+const ButtonModalComponent: FC<{onPress: () => void}> = (props) => {
+
+    const pressHandler = () => {
+        props.onPress();
+    }
+    
+    return (
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                style={styles.buttonInnerContainer}
+                onPress={pressHandler}
+            >
+                <Text style={styles.buttonText}>{props.children}</Text>
+            </Pressable>
+        </View>
+    )
+};
+
+export default ButtonModalComponent;
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        margin: 4,
+        borderRadius: 24
+    },
+
+    buttonInnerContainer: {
+        backgroundColor: '#1699B1',
+        paddingVertical: 8,
+        paddingHorizontal: 40,
+        borderRadius: 10
+    },
+
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    }
+})

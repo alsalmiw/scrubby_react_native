e5b175858cf058ec7b684d9dc1ab53cffbf0710a
Daniel Bautista <dbautista9015@gmail.com>
2022-04-13 10:42:34 -0700

    Just clicked on format document

--- components/AddPhotoComponent.tsx
+++ components/AddPhotoComponent.tsx
@@ -2,33 +2,33 @@ import React from "react";
 import { FC, useContext } from "react";
 import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
 import { FontAwesome } from '@expo/vector-icons';
-import {ThemeContext} from "../context/ThemeContext"
+import { ThemeContext } from "../context/ThemeContext"
 
-interface PhotoType{
+interface PhotoType {
     photo: string;
 }
 
-const AddPhotoComponent: FC<PhotoType> =({photo}) => {
-    const {lilacColor} = useContext(ThemeContext)
+const AddPhotoComponent: FC<PhotoType> = ({ photo }) => {
+    const { lilacColor } = useContext(ThemeContext)
 
-    return(
+    return (
         <TouchableHighlight style={[styles.container]}>
-            
-            <FontAwesome name="camera" color={lilacColor} size={25}  />
+
+            <FontAwesome name="camera" color={lilacColor} size={25} />
         </TouchableHighlight>
     )
 }
 
 const styles = StyleSheet.create({
-        container: {
-            alignItems: 'center',
-            justifyContent: 'center',
-             width:100,
-             height:100,
-            padding: 10,
-            margin: 3,
-            
-        },
-    })
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 100,
+        height: 100,
+        padding: 10,
+        margin: 3,
+
+    },
+})
 
 export default AddPhotoComponent

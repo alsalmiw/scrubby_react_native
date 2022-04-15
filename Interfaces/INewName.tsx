4f406b5b7bc5996cb9305050641cb6223d0a25c2
alsalmiw <alsalmiw@gmail.com>
2022-04-15 13:22:33 -0700

    changed update name to sending dto - walaa

---
+++ Interfaces/INewName.tsx
@@ -0,0 +1,4 @@
+export default interface INewName{
+    Username:string;
+    Name:string;
+}
--- screens/Settings/EditProfileScreen.tsx
+++ screens/Settings/EditProfileScreen.tsx
@@ -1,5 +1,5 @@
 // import { StatusBar } from 'expo-status-bar';
-import { FC, useState } from 'react';
+import { FC, useContext, useState } from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import AddPhotoComponent from '../../components/AddPhotoComponent';
@@ -8,6 +8,9 @@ import InputFieldComponent from '../../components/AddEdit/InputFieldComponent'
 import TitleComponent from '../../components/AddEdit/TitleComponent'
 import FullButtonComponent from '../../components/FullButtonComponent'
 import WhiteSubTitleComponent from '../../components/AddEdit/WhiteSubTitleComponent';
+import {UpdateName} from '../../services/dataService'
+import UserContext from '../../context/UserContext';
+import INewName from '../../Interfaces/INewName'
 
 
 type Props = NativeStackScreenProps <RootStackParamList, 'EditProfile'>
@@ -16,9 +19,26 @@ const EditProfileScreen: FC<Props> = ({navigation, route})=> {
   
   const [newName, setNewName] = useState('')
   const [newPassword, setNewPassword] = useState('')
+  const {username, userData} = useContext(UserContext)
+
+
+  const saveName = async () => {
+    let data:INewName = {
+      Username: "Walaa",
+      Name: newName
+    }
+    console.log(data)
+    let result = await UpdateName(data)
+    if(result) {
+        console.log (result)
+    }
+
+  }
 
   const handleSave = () => {
+    saveName()
     console.log(newName, newPassword)
+
   }
   
   return (

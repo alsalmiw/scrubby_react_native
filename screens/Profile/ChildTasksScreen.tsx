3c7f59d06988045b6ce7bfa7fc68964651f7b676
pVang8 <pvang@codestack.co>
2022-05-13 11:16:22 -0700

    clean code -peter

--- screens/Profile/ChildTasksScreen.tsx
+++ screens/Profile/ChildTasksScreen.tsx
@@ -20,11 +20,11 @@ import { GetTasksByRoomId } from '../../services/dataService';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'ChildTasks'>
 
-const ChildTasksScreen: FC<Props> = ({navigation}) => {
+const ChildTasksScreen: FC<Props> = ({ navigation }) => {
   const { childPage, setChildPage, userData, rState, mySpace, setTasks, setMyRoom, setModalVisible } = useContext(UserContext)
 
 
-  const [childTasks, setChildTasks]= useState<object>([])
+  const [childTasks, setChildTasks] = useState<object>([])
   let newArr = ['bed', 'bathroom', 'kitchen']
   let r = Math.floor(Math.random() * 7)
 
@@ -63,7 +63,7 @@ const ChildTasksScreen: FC<Props> = ({navigation}) => {
           <View style={styles.firstRow}>
             <AvatarComponent onPress={() => console.log('hi')} imageSource={userData.photo} />
           </View>
-          
+
 
           <View style={styles.nameAndCoinContainer}>
             <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'space-evenly', }}>
@@ -71,7 +71,7 @@ const ChildTasksScreen: FC<Props> = ({navigation}) => {
             </View>
 
             <Text>{childPage.dependentAge} years old</Text>
-            
+
 
             <View style={styles.coinContainer}>
               <CoinsPointsDisplayContainer coins={childPage.dependentCoins} points={childPage.dependentPoints} ></CoinsPointsDisplayContainer>
@@ -79,18 +79,18 @@ const ChildTasksScreen: FC<Props> = ({navigation}) => {
 
 
           </View>
-          <View style={{ flex: 0, alignItems: 'flex-start', justifyContent: 'flex-start', paddingTop: 10, paddingRight:10, marginTop: 10, height: 100 }}>
-          <Pressable onPress={() => {setModalVisible(true), console.log('ll')}}>
-            <FontAwesome5 name="unlock" size={40} color="black" />
-          </Pressable>
-          <ChildLockModalComponent />
-        </View>
+          <View style={styles.unlockIconView}>
+            <Pressable onPress={() => { setModalVisible(true), console.log('ll') }}>
+              <FontAwesome5 name="unlock" size={40} color="black" />
+            </Pressable>
+            <ChildLockModalComponent />
+          </View>
 
 
 
         </View>
 
-        <View style={{ paddingLeft: 10, paddingRight: 10, justifyContent: 'center' }}>
+        <View style={styles.underLineView}>
           <UnderlinedOneHeaderComponent titleFirst={'My Rooms'}></UnderlinedOneHeaderComponent>
         </View>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
@@ -98,7 +98,7 @@ const ChildTasksScreen: FC<Props> = ({navigation}) => {
 
         </ScrollView>
 
-        <View style={{ paddingLeft: 10, paddingRight: 10, justifyContent: 'center' }}>
+        <View style={styles.underLineView}>
           <UnderlinedOneHeaderComponent titleFirst={'Tasks'}></UnderlinedOneHeaderComponent>
 
         </View>
@@ -139,6 +139,20 @@ const styles = StyleSheet.create({
   },
   buttonSize: {
     width: 50, height: 50
+  },
+  underLineView: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    justifyContent: 'center'
+  },
+  unlockIconView: {
+    flex: 0,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    paddingTop: 10,
+    paddingRight: 10,
+    marginTop: 10,
+    height: 100
   },
 });
 

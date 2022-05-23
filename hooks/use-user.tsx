1608e49b21e6b14cccae8082f51cf7d5645d181d
Daniel Bautista <dbautista9015@gmail.com>
2022-05-22 21:55:33 -0700

    formatted code

--- hooks/use-user.tsx
+++ hooks/use-user.tsx
@@ -1,11 +1,11 @@
-import {useState} from "react"
+import { useState } from "react"
 import { ISpace } from "../Interfaces/ISpace"
 import IUserData from "../Interfaces/IUserData"
 import IChild from "../Interfaces/IChild"
 import ITask from "../Interfaces/ITask"
 import IScoreBoard from "../Interfaces/IScoreBoard"
 
-export default function UseUser(){
+export default function UseUser() {
 
     interface newSpace {
         collectionName: string,
@@ -13,17 +13,17 @@ export default function UseUser(){
         isDeleted: boolean,
         userId: number
     }
-    interface IRoom{
+    interface IRoom {
         id: number,
         spaceName: string,
         spaceCategory: string,
         collectionId: number,
     }
-   interface ISelectedUser {
-       id: number,
-       fullName: string,
-       isChild:boolean
-   }
+    interface ISelectedUser {
+        id: number,
+        fullName: string,
+        isChild: boolean
+    }
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
@@ -37,8 +37,8 @@ export default function UseUser(){
     const [childData, setChildData] = useState<IChild[]>([])
     const [childPage, setChildPage] = useState<IChild>()
     const [childrenData, setChildrenData] = useState<IChild[]>([])
-    const [mySpaces, setMySpaces] =useState<newSpace[]>([])
-    const [mySpace, setMySpace] =useState<ISpace[]>([])
+    const [mySpaces, setMySpaces] = useState<newSpace[]>([])
+    const [mySpace, setMySpace] = useState<ISpace[]>([])
     const [newSpace, setNewSpace] = useState<ISpace[]>([]);
     const [inviters, setInviters] = useState<any>([])
     const [invited, setInvited] = useState<any>([])
@@ -57,8 +57,8 @@ export default function UseUser(){
     const [myRooms, setMyRooms] = useState<IRoom[]>([])
     const [tasksAPI, setTasksAPI] = useState([])
     const [myRoom, setMyRoom] = useState<IRoom>()
-    const [roomTasks, setRoomTasks]=useState<ITask[]>([]);
-    const [scoreBoardList, setScoreBoardList]=useState<IScoreBoard[]>([]);
+    const [roomTasks, setRoomTasks] = useState<ITask[]>([]);
+    const [scoreBoardList, setScoreBoardList] = useState<IScoreBoard[]>([]);
     const [refresh, setRefresh] = useState<boolean>()
     const [spinnerOn, setSpinnerOn] = useState<boolean>()
 
@@ -66,20 +66,20 @@ export default function UseUser(){
     const [selectedUser, setSelectedUser] = useState<ISelectedUser>()
     const [modalVisible, setModalVisible] = useState(false);
     const [taskModal, setTaskModal] = useState(false);
-    const [scheduleTask, setScheduleTask]= useState()
+    const [scheduleTask, setScheduleTask] = useState()
 
     const [childPassCode, setChildPassCode] = useState<number>(0)
     const [checkPassCode, setCheckPassCode] = useState<boolean>(true)
- const [defaultSpace, setDefaultSpace] = useState<any[]>([])
- const [childRooms, setChildRooms] = useState<any>([])
+    const [defaultSpace, setDefaultSpace] = useState<any[]>([])
+    const [childRooms, setChildRooms] = useState<any>([])
 
-    return{username, setUsername, password, setPassword, seeAll, setSeeAll, savedUsername, setSavedUsername, savedPassword, setSavedPassword, isChildFree, setIsChildFree,  userData, setUserData, childData, setChildData, mySpaces, setMySpaces, myRooms, setMyRooms, task, setTask, allTask, setAllTask, addTask, setAddTask, mySpace, setMySpace, rState, setRState, invited, setInvited, inviters, setInviters, setChildrenData, childrenData, setNewSpace, usersAddedTasks, setUsersAddedTasks, myRoom, setMyRoom, tasksAPI, setTasksAPI, roomTasks, setRoomTasks, fullUserInfo, setFullUserInfo, refresh, setRefresh, scoreBoardList, setScoreBoardList, acceptedInvitations, setAcceptedInvitations, taskUser, setTaskUser,spinnerOn, setSpinnerOn, childPage, setChildPage, isChild, setIsChild, selectedUser, setSelectedUser, modalVisible, setModalVisible, scheduleTask, setScheduleTask, childPassCode, setChildPassCode, checkPassCode, setCheckPassCode,  defaultSpace, setDefaultSpace,  taskModal, setTaskModal, childRooms, setChildRooms}
-
-   
-    
-    
+    return { username, setUsername, password, setPassword, seeAll, setSeeAll, savedUsername, setSavedUsername, savedPassword, setSavedPassword, isChildFree, setIsChildFree, userData, setUserData, childData, setChildData, mySpaces, setMySpaces, myRooms, setMyRooms, task, setTask, allTask, setAllTask, addTask, setAddTask, mySpace, setMySpace, rState, setRState, invited, setInvited, inviters, setInviters, setChildrenData, childrenData, setNewSpace, usersAddedTasks, setUsersAddedTasks, myRoom, setMyRoom, tasksAPI, setTasksAPI, roomTasks, setRoomTasks, fullUserInfo, setFullUserInfo, refresh, setRefresh, scoreBoardList, setScoreBoardList, acceptedInvitations, setAcceptedInvitations, sentAcceptedInvitations ,setSentAcceptedInvitations, taskUser, setTaskUser, spinnerOn, setSpinnerOn, childPage, setChildPage, isChild, setIsChild, selectedUser, setSelectedUser, modalVisible, setModalVisible, scheduleTask, setScheduleTask, childPassCode, setChildPassCode, checkPassCode, setCheckPassCode, defaultSpace, setDefaultSpace, taskModal, setTaskModal, childRooms, setChildRooms }
 
 
 
-    
+
+
+
+
+
 }

60835e310affdf372d092d3e28ebb797741f9db6
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 11:00:06 -0700

    format doc, TaskSpaceRowComp

--- components/TaskSpaceRowComponent.tsx
+++ components/TaskSpaceRowComponent.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode, useEffect, useState } from "react";
 import { FC, useContext } from "react";
 import { Pressable, StyleSheet } from "react-native";
-import {ThemeContext} from "../context/ThemeContext"
+import { ThemeContext } from "../context/ThemeContext"
 
 interface Props {
     children: ReactNode,
@@ -9,45 +9,41 @@ interface Props {
     onPress: Function | undefined
 }
 
-const TaskSpaceRowComponent: FC<Props> =(props) => {
-    const {orangeColor, purpleColor, fuchsiaColor, violetColor, greenColor, yellowColor, blueColor} = useContext(ThemeContext)
-    const [bgColor, setBgColor]= useState('')
+const TaskSpaceRowComponent: FC<Props> = (props) => {
+    const { orangeColor, purpleColor, fuchsiaColor, violetColor, greenColor, yellowColor, blueColor } = useContext(ThemeContext)
+    const [bgColor, setBgColor] = useState('')
 
-  useEffect(()=> {
-      selectColor()
-    // console.log(props.idx)
-},[])
+    useEffect(() => {
+        selectColor()
+    }, [])
 
     const selectColor = () => {
-        //let r = Math.floor (Math.random()*7)
-        let index =0
+
+        let index = 0
         let colors = [orangeColor, purpleColor, fuchsiaColor, violetColor, greenColor, yellowColor, blueColor]
         index = props.idx % colors.length
-        // console.log(index)
-        setBgColor(colors[index]) 
+
+        setBgColor(colors[index])
     }
     const handleOnPress = () => {
-        if(props.onPress) 
-        {props.onPress()}
+        if (props.onPress) { props.onPress() }
     }
 
-    return(
-        <Pressable key ={props.idx} style={[styles.container, {backgroundColor:bgColor}]} onPress={()=>handleOnPress()}>
-            
+    return (
+        <Pressable key={props.idx} style={[styles.container, { backgroundColor: bgColor }]} onPress={() => handleOnPress()}>
             {props.children}
-
         </Pressable>
     )
 }
 
 const styles = StyleSheet.create({
-       container: {
-            width:"95%",
-            padding: 10,
-            borderRadius:5,
-            margin: 3,
-          
-        },
-    })
+    container: {
+        width: "95%",
+        padding: 10,
+        borderRadius: 5,
+        margin: 3,
+
+    },
+})
 
 export default TaskSpaceRowComponent

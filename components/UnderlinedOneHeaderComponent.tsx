3c55bf612ef9b4df988643245f3b3da423e29586
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 11:07:32 -0700

    format doc, UnderlinedOneHeaderComp

--- components/UnderlinedOneHeaderComponent.tsx
+++ components/UnderlinedOneHeaderComponent.tsx
@@ -1,39 +1,37 @@
 import React from "react";
 import { FC, useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import {ThemeContext} from "../context/ThemeContext"
+import { ThemeContext } from "../context/ThemeContext"
 import UserContext from "../context/UserContext";
 
-interface TitleType{
+interface TitleType {
     titleFirst: string;
 
 }
 
-const UnderlinedOneHeaderComponent: FC<TitleType> =({titleFirst}) => {
-    const {secondaryTextColor, lightLilacColor} = useContext(ThemeContext)
+const UnderlinedOneHeaderComponent: FC<TitleType> = ({ titleFirst }) => {
+    const { secondaryTextColor, lightLilacColor } = useContext(ThemeContext)
 
-
-
-    return(
-        <View style={[styles.headerContainer, {borderBottomColor: lightLilacColor} ]}>
-        <Text style={[styles.mainHeader, {color:secondaryTextColor}]}>{titleFirst}</Text>
+    return (
+        <View style={[styles.headerContainer, { borderBottomColor: lightLilacColor }]}>
+            <Text style={[styles.mainHeader, { color: secondaryTextColor }]}>{titleFirst}</Text>
         </View>
     )
 }
 
 
 const styles = StyleSheet.create({
-        mainHeader: {
-            fontSize:20,
-            fontWeight: "bold", 
-        }, 
-        headerContainer:{
-            width:'100%',
-            flexDirection: "row",
-            justifyContent: "space-between",
-            borderBottomWidth:1,
-        
-        }
-    })
+    mainHeader: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    headerContainer: {
+        width: '100%',
+        flexDirection: "row",
+        justifyContent: "space-between",
+        borderBottomWidth: 1,
+
+    }
+})
 
 export default UnderlinedOneHeaderComponent 

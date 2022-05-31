1e5cc1920a970790d6079a39b610e61be540106a
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 12:53:39 -0700

    format, comment, InviteUserPendingScreen

--- screens/Settings/InviteUserPendingScreen.tsx
+++ screens/Settings/InviteUserPendingScreen.tsx
@@ -1,6 +1,6 @@
-import {  FC, useContext, useEffect, useState } from 'react';
+import { FC, useContext, useEffect, useState } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import {  StyleSheet, Text, View, StatusBar, Dimensions, Pressable, Alert } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions, Pressable, Alert } from 'react-native';
 import { ThemeContext } from '../../context/ThemeContext';
 import UserContext from '../../context/UserContext';
 
@@ -18,39 +18,28 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { DeleteInvite } from '../../services/dataService';
 
 
-
-
-
 type Props = NativeStackScreenProps<RootStackParamList, 'InviteUserPending'>
 
-const InviteUserPendingScreen:FC<Props> = ({navigation}) => {
+const InviteUserPendingScreen: FC<Props> = ({ navigation }) => {
 
-
-
-    const { purpleColor, lilacColor} = useContext(ThemeContext)
-    const {invited, setInvited, allRequestName, setAllRequestName, userData, refresh, setRefresh} =useContext(UserContext)
+    const { purpleColor, lilacColor } = useContext(ThemeContext)
+    const { invited, setInvited, allRequestName, setAllRequestName, userData, refresh, setRefresh } = useContext(UserContext)
     const [fullName, setFullName] = useState<string | null>("");
     const [invitedPhoto, setInvitedPhoto] = useState<string>("?")
 
 
-    const showName = async() => {
-         let invitedUserFromAsyncStorage = await AsyncStorage.getItem('Invited');
-    //     AsyncStorage.setItem('Inviter', userData.username);
-    //     let inviter = await AsyncStorage.getItem('Inviter');
-
-         //console.log(invited);
-    //    console.log(userData)
-    //     setName(invited);
+    const showName = async () => {
+        let invitedUserFromAsyncStorage = await AsyncStorage.getItem('Invited');
 
         invited.forEach((person: any) => {
             if (person.invitedUsername === invitedUserFromAsyncStorage) {
-                
+
                 if (person.invitedFullname === null) {
                     setFullName(person.invitedUsername)
                 } else {
                     setFullName(person.invitedFullname)
                 }
-                
+
                 setInvitedPhoto(person.invitedPhoto);
             }
         });
@@ -63,103 +52,66 @@ const InviteUserPendingScreen:FC<Props> = ({navigation}) => {
     }
 
     const handleDeleteInvite = async () => {
-        
 
-
-        //console.log(testArray.filter((element: any) => element.invitedFullname !== name));
-        // setAllInvites((currentInvites:any) => currentInvites.filter((person: any) => {
-
-        //     if (person.invitedUsername) {
-                
-        //         const callDeleteUser = async () => {
-        //             let result = await DeleteInvite(userData.id, person.invitedUsername);
-                   
-        //         }
-        //         callDeleteUser();
-
-        //         return person.invitedFullname !== name
-        //     } else {
-        //         return person.invitedUsername !== name
-        //     }
-
-            
-            
-        // }))
-
-        // let testArray = invited;
-        // let invitedUserFromAsyncStorage = await AsyncStorage.getItem('Invited');
-
-        // console.log(testArray.filter((invitee: any) => invitee.invitedUsername !== invitedUserFromAsyncStorage));
 
         let invitedUserToBeDeleted = await AsyncStorage.getItem('Invited');
-        
-        const DeleteInviteFetch = async() => {
+
+        const DeleteInviteFetch = async () => {
             await DeleteInvite(userData.id, invitedUserToBeDeleted!);
-            setRefresh((prevRefresh:boolean) => prevRefresh = true)
+            setRefresh((prevRefresh: boolean) => prevRefresh = true)
         }
-        
+
         DeleteInviteFetch();
-        
+
 
         navigation.navigate('ManageInvites');
 
-        
+
     }
 
     const handleDisplayAlert = () => {
-     //   console.log('This item is fake deleted');
-        Alert.alert('Warning', 'This action will result in deleting the user and all of their chores will be deleted. Are you sure you want to delete this user?',[ {
+
+        Alert.alert('Warning', 'This action will result in deleting the user and all of their chores will be deleted. Are you sure you want to delete this user?', [{
             text: "Cancel",
-            onPress: () => console.log("Cancel Pressed"),
+            onPress: () => {},
             style: "cancel"
-          },
-          { text: "Delete", onPress: handleDeleteInvite , style: "destructive" }
+        },
+        { text: "Delete", onPress: handleDeleteInvite, style: "destructive" }
         ]);
-        
+
     }
 
-    
-
-    
-
-    useEffect(()=> {
+    useEffect(() => {
         showName()
     }, [])
-
-    
-
-
-    
-
-    
 
     return (
         <View style={styles.container}>
             <View>
-            <HeaderComponent title="Add To My Space" />
-            
-            <View style={styles.firstRowContainer}>
-                <AvatarComponent onPress={undefined} imageSource={invitedPhoto}/>
-                <View style={styles.insideFirstRowContainer1}>
-                    <UserNameComponent name={fullName}></UserNameComponent>
-                    <Pressable style={styles.insideFirstRowContainer2} onPress={handleDisplayAlert}>
-                        <Feather name="trash-2" size={30} color={lilacColor} />
-                        <UserNameComponent name="Delete User"></UserNameComponent>
-                    </Pressable>
+                <HeaderComponent title="Add To My Space" />
+
+                <View style={styles.firstRowContainer}>
+                    <AvatarComponent onPress={undefined} imageSource={invitedPhoto} />
+                    <View style={styles.insideFirstRowContainer1}>
+                        <UserNameComponent name={fullName}></UserNameComponent>
+                        <Pressable style={styles.insideFirstRowContainer2} onPress={handleDisplayAlert}>
+                            <Feather name="trash-2" size={30} color={lilacColor} />
+                            <UserNameComponent name="Delete User"></UserNameComponent>
+                        </Pressable>
+                    </View>
+                </View>
+
+                <View style={styles.secondRowContainer}>
+                    <UnderlinedOneHeaderComponent titleFirst='Add To'></UnderlinedOneHeaderComponent>
+                    <View style={styles.insideSecondRowContainer1}>
+                        <UserNameComponent name="This user has not accepted your invitation yet to share responsibilities."></UserNameComponent>
+                    </View>
                 </View>
             </View>
-            
-            <View style={styles.secondRowContainer}>
-                <UnderlinedOneHeaderComponent titleFirst='Add To'></UnderlinedOneHeaderComponent>
-                <View style={styles.insideSecondRowContainer1}>
-                    <UserNameComponent name="This user has not accepted your invitation yet to share responsibilities."></UserNameComponent>
-                </View>
-            </View>
-            </View>
-            <FullButtonComponent  radius={0} onPress={handleNavigateBack} color={purpleColor}>
+            <FullButtonComponent radius={0} onPress={handleNavigateBack} color={purpleColor}>
                 <Text>Back</Text>
             </FullButtonComponent>
-            
+
 
         </View>
     )
@@ -171,7 +123,7 @@ export default InviteUserPendingScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-       justifyContent:"space-between"
+        justifyContent: "space-between"
     },
     firstRowContainer: {
         flexDirection: 'row',
@@ -187,10 +139,10 @@ const styles = StyleSheet.create({
     secondRowContainer: {
         marginTop: '5%',
         paddingLeft: '3%',
-        
+
     },
     insideSecondRowContainer1: {
         marginTop: '3%'
     }
-    
+
 })

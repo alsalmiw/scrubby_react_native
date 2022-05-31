e24764b07f7f57fba699c3ac66d00131b43b6d93
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 09:57:37 -0700

    Formatted document CoinsPointsDisplay

--- components/Profile/CoinsPointsDisplayContainer.tsx
+++ components/Profile/CoinsPointsDisplayContainer.tsx
@@ -1,36 +1,35 @@
 import React from "react";
 import { FC, useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import {ThemeContext} from "../../context/ThemeContext"
+import { ThemeContext } from "../../context/ThemeContext"
 import { MaterialCommunityIcons, Ionicons, FontAwesome, FontAwesome5 } from '@expo/vector-icons';
 
-interface NameType{
+interface NameType {
     coins: string;
     points: string;
 }
 
-const CoinsPointsDisplayContainer: FC<NameType> =({coins, points}) => {
-    const {secondaryTextColor, yellowColor, lilacColor} = useContext(ThemeContext)
+const CoinsPointsDisplayContainer: FC<NameType> = ({ coins, points }) => {
+    const { secondaryTextColor, yellowColor, lilacColor } = useContext(ThemeContext)
 
-    return(
-        <View style={[styles.coinsDisplayContainer, {borderColor:yellowColor}]}>
-            
-            <Text style={[styles.fontStyle, {color:secondaryTextColor}]}> <FontAwesome name="star" color={yellowColor} />  {points}</Text>
-            <Text style={[styles.fontStyle, {color:secondaryTextColor}]}> <FontAwesome5 name="coins" color={yellowColor} />  {coins}</Text>
+    return (
+        <View style={[styles.coinsDisplayContainer, { borderColor: yellowColor }]}>
+            <Text style={[styles.fontStyle, { color: secondaryTextColor }]}> <FontAwesome name="star" color={yellowColor} />  {points}</Text>
+            <Text style={[styles.fontStyle, { color: secondaryTextColor }]}> <FontAwesome5 name="coins" color={yellowColor} />  {coins}</Text>
         </View>
-        
+
     )
 }
 
 const styles = StyleSheet.create({
     coinsDisplayContainer: {
-            width: '40%',
-            borderWidth:2,
-            borderRadius:5,
-        },
-        fontStyle:{
+        width: '40%',
+        borderWidth: 2,
+        borderRadius: 5,
+    },
+    fontStyle: {
 
-        }
-    })
+    }
+})
 
 export default CoinsPointsDisplayContainer

f38c52e640458ca38e0df050fe55de1d2839e5cb
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 12:36:27 -0700

    format, remove comment, log, MyProfileScreen

--- screens/Profile/MyProfileScreen.tsx
+++ screens/Profile/MyProfileScreen.tsx
@@ -1,4 +1,3 @@
-// import { StatusBar } from 'expo-status-bar';
 import { FC, useContext, useState, useEffect } from 'react';
 import { ScrollView, StyleSheet, Text, View, StatusBar, FlatList, Pressable } from 'react-native';
 import CoinsPointsDisplayContainer from '../../components/Profile/CoinsPointsDisplayContainer'
@@ -15,7 +14,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { GetSpaceCollectionByUserId, GetUserByUsername, GetDependantByUserId, GetSpacesByCollectionID, GetSelectedTasksByUserID, GetChildDefaultSchedule, GetDependantsDTOByUserId } from '../../services/dataService';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-//This is just testing
+
 import { Dimensions } from 'react-native';
 
 import { Entypo } from '@expo/vector-icons';
@@ -45,14 +44,12 @@ const MyProfileScreen: FC<Props> = ({ navigation }) => {
   const { bgColor, lilacColor, primaryTextColor } = useContext(ThemeContext)
   const { savedUsername, setSavedUsername, isChildFree, userData, setUserData, childData, setChildData, myRooms, setMyRooms, setMySpace, setMySpaces, mySpaces, childrenData, setChildrenData, setUsersAddedTasks, setChildPage, childPage, childDefaultSpace, setChildDefaultSpace, setBlank, memberInfo, setMemberInfo, setIsEditImage, setRunAgain, childrenInfo, setChildrenInfo, myHouses, setMyHouses, setChildCoins, setChildPoints, setWaiting, waiting } = useContext(UserContext)
 
-
-  //This is a test useState for populating create a new space
   const [newSpace, setNewSpace] = useState<newSpace[]>([]);
   const [r, setR] = useState<number>(Math.floor(Math.random() * 7))
 
 
   const handleAddChild = () => {
-    //console.log('Plus Icon Works');
+    
     navigation.navigate('AddChild')
   }
 
@@ -62,47 +59,33 @@ const MyProfileScreen: FC<Props> = ({ navigation }) => {
   }
 
   const handleGoToSpaceRooms = async (space: any) => {
-    // console.log("collection id is "+space.id)
-    let spaceRooms = await GetSpacesByCollectionID(space.id)
-    //console.log(spaceRooms)
-    // console.log("spacerooms" + spaceRooms)
-   
-
-    if (spaceRooms!= null || spaceRooms.length != 0) {
-      setMyRooms(spaceRooms)
-     navigation.navigate('Rooms')
     
-      //console.log("in the if statement")
+    let spaceRooms = await GetSpacesByCollectionID(space.id)
+    
+
+
+    if (spaceRooms != null || spaceRooms.length != 0) {
+      setMyRooms(spaceRooms)
+      navigation.navigate('Rooms')
+
+      
     }
     else {
       setMyRooms('')
-      //console.log("in the else statement")
-     
-     navigation.navigate('Rooms')
+      
+
+      navigation.navigate('Rooms')
     }
   }
 
 
-  // const asyncShit = async (Id: number) => {
-  //   let result = await GetSpaceCollectionById(Id);
-  //   setNewSpace(prevState => {
-  //     return [...prevState, result];
-  //   });
-  //   console.log(result);
-  // }
-
   useEffect(() => {
 
-    
+
     navigation.addListener('focus', () => {
       setBlank(false)
       setRunAgain(true)
     })
-
-
-    // console.log(savedUsername)
-    //AsyncGetSpaceCollectionById();
-    //AsyncGetSpaceCollectionById();
 
 
 
@@ -110,71 +93,27 @@ const MyProfileScreen: FC<Props> = ({ navigation }) => {
 
 
 
-  // const AsyncGetSpaceCollectionById = async () => {
-
-  //   let userInfo: any = await AsyncStorage.getItem("Username");
-  //   if (userInfo) {
-  //     setSavedUsername(userInfo)
-  //     //console.log(userInfo)
-  //   }
-
-  //   let user = await GetUserByUsername(savedUsername)
-  //   //console.log(user);
-  //   // console.log(user)
-  //   if (user.length != 0) {
-  //     setUserData(user)
-  //     let result = await GetSpaceCollectionByUserId(user.id);
-  //     // let children = await GetDependantByUserId(user.id);
-  //     //let usersTasks = await GetSelectedTasksByUserID(user.id)
-  //     //console.log(children)
-  //     if (result.length != 0) {
-  //       setNewSpace([...result])
-  //       setMySpaces(result)
-  //     }
-  //     // if (children.length != 0) {
-  //     //   console.log(children.length);
-  //     //   setChildrenData(children)
-  //     //   console.log("after");
-  //     // }
-  //     //   if(usersTasks.length!= 0)
-  //     // {
-  //     //   setUsersAddedTasks (usersTasks)
-  //     //   console.log(usersTasks)
-  //     // }
-
-  //   }
-
-  // }
-
-
   const handleGoToChildProfile = async (child: any) => {
     setWaiting(true)
-    // console.log("=======================================================================++")
-    //console.log(child)
 
     await setChildPage(child)
     await setChildCoins(child.dependentCoins)
     await setChildPoints(child.dependentPoints)
     let childDefault = await GetChildDefaultSchedule(child.id)
-    //console.log("Child fetch", childDefault)
+    
     if (childDefault.length != 0) {
       setChildDefaultSpace(childDefault)
       navigation.navigate('ChildTasks')
       setWaiting(false)
-      //console.log("got it");
+      
     }
     else {
       setChildDefaultSpace([])
-      //console.log("child Page",childPage)
+      
       setWaiting(false)
       navigation.navigate('ChildTasks')
-      //console.log('empty default space')
-      // console.log("Child Default Space",childDefaultSpace)
+      
     }
-
-
-    // console.log(child.scheduledTasks)
-    //setChildData(child)
 
   }
 
@@ -230,11 +169,11 @@ const MyProfileScreen: FC<Props> = ({ navigation }) => {
           <View style={styles.underlineContainer}>
             <UnderlinedOneHeaderComponent titleFirst='My Spaces' />
           </View>
-          {/* <UnderlinedHeaderComponent titleOne="My Spaces" titleTwo="" titleThree="" /> */}
+
           <Pressable style={styles.secondRow} onPress={handleAddNewSpaceNavigation}>
-     
-              <Entypo name="squared-plus" size={50} color={lilacColor} />
-       
+
+            <Entypo name="squared-plus" size={50} color={lilacColor} />
+
             <View style={styles.userNameContainer}>
               <UserNameComponent name="Create a New Space"></UserNameComponent>
             </View>
@@ -252,8 +191,6 @@ const MyProfileScreen: FC<Props> = ({ navigation }) => {
                   {space.collectionName}
                 </TaskSpaceRowTrash>
 
-
-
               )
               : null
             }
@@ -265,7 +202,7 @@ const MyProfileScreen: FC<Props> = ({ navigation }) => {
                 <View style={styles.underlineContainer} >
                   <UnderlinedOneHeaderComponent titleFirst='Kids' />
                 </View>
-                {/* <UnderlinedHeaderComponent titleOne="Kids" titleTwo="" titleThree="" /> */}
+
                 <View style={styles.thirdRow}>
 
                   <AddItemButtonComponent onPress={handleAddChild}>
@@ -275,9 +212,7 @@ const MyProfileScreen: FC<Props> = ({ navigation }) => {
                   {childrenData.length != 0 ?
                     childrenData.map((child: any, idx: number) => {
                       return (
-                        //     <AddItemButtonComponent key={idx} onPress={()=>handleGoToChildProfile(child)}>
-                        //   <Entypo name="squared-cross" size={windowWidth} color={lilacColor} />
-                        // </AddItemButtonComponent>
+
                         <AvatarComponent key={idx} onPress={() => { handleGoToChildProfile(child) }} imageSource={child.dependentPhoto} />
                       )
 
@@ -296,11 +231,6 @@ const MyProfileScreen: FC<Props> = ({ navigation }) => {
     </>
 
 
-
-
-
-
-
   );
 }
 
@@ -308,8 +238,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+
     paddingTop: StatusBar.currentHeight,
 
   },
@@ -353,8 +282,8 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
-    paddingLeft:'2.5%',
-    paddingRight:'2.5%',
+    paddingLeft: '2.5%',
+    paddingRight: '2.5%',
     alignItems: 'center'
   },
   textStyle: {

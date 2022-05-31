769d6378384f3ba9154f3673c0f5bec337548db9
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 10:51:21 -0700

    format and spacing, TaskRowFullInfoComp

--- components/TaskRowFullInfoComponent.tsx
+++ components/TaskRowFullInfoComponent.tsx
@@ -6,54 +6,51 @@ import { FontAwesome5 } from '@expo/vector-icons';
 import UserContext from '../context/UserContext';
 
 interface taskProp {
-    task: ITask; 
+    task: ITask;
     idx: number;
-    r:number
+    r: number
 }
 
 
-const TaskRowFullInfoComponent: FC<taskProp> = ({task, idx, r}) => {
-    const { current, setCurrent} = useContext(UserContext)
+const TaskRowFullInfoComponent: FC<taskProp> = ({ task, idx, r }) => {
+    const { current, setCurrent } = useContext(UserContext)
     const [showTask, setShowTask] = useState(false)
 
-    const displayTaskInfo =()=> {
-       setCurrent(task.id)
+    const displayTaskInfo = () => {
+        setCurrent(task.id)
     }
 
-  return (
-    <View key={idx}>
-    <TaskSpaceRowComponent idx={r+idx} onPress={()=>{displayTaskInfo(); }}>
-        <View style={[styles.container, styles.flexrow]}>
-          <Text style={[styles.text, ]}>{task.task.name} {task.item.name}</Text>
-          <View style={[styles.flexrow]}>
-          <FontAwesome5 name={'coins'} size={25} style={{marginRight: 10, color: "#FFF"}} />
-          <Text style={[styles.text, ]}>{task.task.coins} coins</Text>
-          </View>
+    return (
+        <View key={idx}>
+            <TaskSpaceRowComponent idx={r + idx} onPress={() => { displayTaskInfo(); }}>
+                <View style={[styles.container, styles.flexrow]}>
+                    <Text style={[styles.text,]}>{task.task.name} {task.item.name}</Text>
+                    <View style={[styles.flexrow]}>
+                        <FontAwesome5 name={'coins'} size={25} style={{ marginRight: 10, color: "#FFF" }} />
+                        <Text style={[styles.text,]}>{task.task.coins} coins</Text>
+                    </View>
+                </View>
+            </TaskSpaceRowComponent>
         </View>
-    </TaskSpaceRowComponent>
-
-    </View>
-  )
+    )
 }
 const styles = StyleSheet.create({
-       container: {
-            justifyContent: 'space-between',
-            padding: 5,
-        },
-        flexrow: {
-            flexDirection: "row"
-        },
-        text: {
-            color:"#FFF", 
-            fontWeight: 'bold', 
-            fontSize: 17
-        }, 
-        taskInfo: {
-            fontSize: 20,
-        }
-        
-       
+    container: {
+        justifyContent: 'space-between',
+        padding: 5,
+    },
+    flexrow: {
+        flexDirection: "row"
+    },
+    text: {
+        color: "#FFF",
+        fontWeight: 'bold',
+        fontSize: 17
+    },
+    taskInfo: {
+        fontSize: 20,
+    }
 
-    })
+})
 
 export default TaskRowFullInfoComponent

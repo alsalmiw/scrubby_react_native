5b4a51bde2ae6b29232ff6bca491b6076bf40697
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 13:03:27 -0700

    format, comment, SettingsNavigation

--- screens/Settings/SettingsNavigation.tsx
+++ screens/Settings/SettingsNavigation.tsx
@@ -1,4 +1,4 @@
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { FC } from 'react';
 import EditProfileScreen from '../Profile/EditProfileScreen'
 import SettingsScreen from '../Settings/SettingsScreen'
@@ -21,28 +21,28 @@ import TasksHistoryScreen from './TasksHistoryScreen';
 
 const Tab = createNativeStackNavigator<RootStackParamList>();
 
-const SettingsNavigation: FC = ()=> {
+const SettingsNavigation: FC = () => {
 
   return (
 
-        <Tab.Navigator  initialRouteName="SettingsScreen">
-          <Tab.Screen name="SettingsScreen" component={SettingsScreen} options={{ headerShown: false }}/>
-          {/* <Tab.Screen name="EditProfile" component={EditProfileScreen} options={{ headerShown: false }}/> */}
-          <Tab.Screen name="ManageInvites" component={ManageInvitesScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="EditProfile" component={EditProfileScreen} options={{ headerShown: false }}/>
-          {/* <Tab.Screen name="ManageInvites" component={ManageInvitesScreen} options={{ headerShown: false}}/> */}
-          <Tab.Screen name="ChangePassword" component={ChangePasswordScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="ScoreBoard" component={ScoreBoardScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="RedeemCoins" component={RedeemCoinsScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="InviteUser" component={InviteUserScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="AcceptRequest" component={AcceptRequestScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="InviteUserPending" component={InviteUserPendingScreen} options={{headerShown: false}} />
-          <Tab.Screen name="AcceptedInvitation" component={AcceptedInvitationScreen} options={{headerShown: false}} />
-          <Tab.Screen name="SentAcceptedInvitation" component={SentAcceptedInvitation} options={{headerShown: false}} />
-          <Tab.Screen name="AcceptedRequest" component={AcceptedRequestScreen} options={{headerShown: false}} />
-          <Tab.Screen name="TasksHistory" component={TasksHistoryScreen} options={{headerShown: false}} />
-          
-        </Tab.Navigator>
+    <Tab.Navigator initialRouteName="SettingsScreen">
+      <Tab.Screen name="SettingsScreen" component={SettingsScreen} options={{ headerShown: false }} />
+
+      <Tab.Screen name="ManageInvites" component={ManageInvitesScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="EditProfile" component={EditProfileScreen} options={{ headerShown: false }} />
+
+      <Tab.Screen name="ChangePassword" component={ChangePasswordScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="ScoreBoard" component={ScoreBoardScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="RedeemCoins" component={RedeemCoinsScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="InviteUser" component={InviteUserScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="AcceptRequest" component={AcceptRequestScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="InviteUserPending" component={InviteUserPendingScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="AcceptedInvitation" component={AcceptedInvitationScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="SentAcceptedInvitation" component={SentAcceptedInvitation} options={{ headerShown: false }} />
+      <Tab.Screen name="AcceptedRequest" component={AcceptedRequestScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="TasksHistory" component={TasksHistoryScreen} options={{ headerShown: false }} />
+
+    </Tab.Navigator>
   );
 }
 

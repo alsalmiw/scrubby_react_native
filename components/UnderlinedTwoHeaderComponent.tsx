2228df0ef61f15d337dc3fef042441d766ed0409
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 11:08:15 -0700

    Format doc, UnderlinedTwoHeaderComp

--- components/UnderlinedTwoHeaderComponent.tsx
+++ components/UnderlinedTwoHeaderComponent.tsx
@@ -1,41 +1,38 @@
 import React from "react";
 import { FC, useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import {ThemeContext} from "../context/ThemeContext"
+import { ThemeContext } from "../context/ThemeContext"
 import UserContext from "../context/UserContext";
 
-interface TitleType{
+interface TitleType {
     titleFirst: string;
     titleTwo: string;
 
 }
 
-const UnderlinedTwoHeaderComponent: FC<TitleType> =({titleFirst, titleTwo}) => {
-    const {secondaryTextColor, lightLilacColor} = useContext(ThemeContext)
+const UnderlinedTwoHeaderComponent: FC<TitleType> = ({ titleFirst, titleTwo }) => {
+    const { secondaryTextColor, lightLilacColor } = useContext(ThemeContext)
 
-
-
-    return(
-        <View style={[styles.headerContainer, {borderBottomColor: lightLilacColor} ]}>
-        <Text style={[styles.mainHeader, {color:secondaryTextColor}]}>{titleFirst}</Text>
-        <Text style={[styles.mainHeader, {color:secondaryTextColor}]}>{titleTwo}</Text>
-
+    return (
+        <View style={[styles.headerContainer, { borderBottomColor: lightLilacColor }]}>
+            <Text style={[styles.mainHeader, { color: secondaryTextColor }]}>{titleFirst}</Text>
+            <Text style={[styles.mainHeader, { color: secondaryTextColor }]}>{titleTwo}</Text>
         </View>
     )
 }
 
 
 const styles = StyleSheet.create({
-        mainHeader: {
-            fontSize:20,
-            fontWeight: "bold", 
-        }, 
-        headerContainer:{
-            width:'100%',
-            flexDirection: "row",
-            justifyContent: "space-between",
-            borderBottomWidth:1,
-        }
-    })
+    mainHeader: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    headerContainer: {
+        width: '100%',
+        flexDirection: "row",
+        justifyContent: "space-between",
+        borderBottomWidth: 1,
+    }
+})
 
 export default UnderlinedTwoHeaderComponent 

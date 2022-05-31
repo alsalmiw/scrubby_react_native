d3838a6302a6171036368e2c9d53c7704975cd10
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 11:11:20 -0700

    Format doc, UserContext

--- context/UserContext.tsx
+++ context/UserContext.tsx
@@ -1,14 +1,14 @@
-import React, {createContext, FC, ReactNode} from 'react'
+import React, { createContext, FC, ReactNode } from 'react'
 import UseUser from '../hooks/use-user';
 
 const UserContext = createContext<any>(UseUser);
 
 export default UserContext;
 
-export const UserProvider: FC <ReactNode> = ({children})=>{
-    return(
+export const UserProvider: FC<ReactNode> = ({ children }) => {
+    return (
         <UserContext.Provider value={UseUser()}>
-        {children}
+            {children}
         </UserContext.Provider>
     )
 }

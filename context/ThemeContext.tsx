e18d5b0f489e2d4e074c5bcfbbd3f22d54858fe3
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 11:10:55 -0700

    Format doc, ThemeContext

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -2,15 +2,12 @@ import React, { FC, ReactNode } from 'react'
 import { createContext } from "react";
 import UseTheme from '../hooks/use-theme'
 
-
-
 export const ThemeContext = createContext<any>(UseTheme);
 
-
-export const ThemeProvider: FC <ReactNode> = ({children})=>{
-    return(
+export const ThemeProvider: FC<ReactNode> = ({ children }) => {
+    return (
         <ThemeContext.Provider value={UseTheme()}>
-        {children}
+            {children}
         </ThemeContext.Provider>
     )
 }

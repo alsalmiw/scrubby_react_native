68f9f8f99e7e704f221a49275059fa602874234f
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 11:44:44 -0700

    format doc, remove comments, log, AddedTaskScreen

--- screens/Profile/AddedtasksScreen.tsx
+++ screens/Profile/AddedtasksScreen.tsx
@@ -18,146 +18,101 @@ import UnderlinedOneHeaderComponent from '../../components/UnderlinedOneHeaderCo
 import TwoFullButtonComponent from '../../components/TwoFullButtonComponent';
 
 
+type Props = NativeStackScreenProps<RootStackParamList, 'AddedTasks'>
 
-type Props = NativeStackScreenProps <RootStackParamList, 'AddedTasks'>
+const AddedTasksScreen: FC<Props> = ({ navigation }) => {
+  const { bgColor, lilacColor, purpleColor } = useContext(ThemeContext)
+  const { userData, usersAddedTasks, setTasksAPI, roomTasks, setRoomTasks, myRoom, tasksAPI, current, setCurrent, setStoredAddedItems, rState, noAddedItems, setNoAddedItems } = useContext(UserContext)
+  const [display, setDisplay] = useState(false)
 
 
-const AddedTasksScreen: FC<Props> = ({navigation})=> {
-    const {bgColor, lilacColor, purpleColor} = useContext(ThemeContext)
-    const { userData, usersAddedTasks, setTasksAPI, roomTasks, setRoomTasks, myRoom, tasksAPI, current, setCurrent, setStoredAddedItems, rState, noAddedItems, setNoAddedItems } = useContext(UserContext)
-    const [display, setDisplay] = useState(false)
-  
+  let r = Math.floor(Math.random() * 7)
 
-    // useEffect(() => {
-  
-     
-    //  //displayTasks()
-     
-  
-    // }, [])
+  const displayAddIcon = () => {
+    navigation.navigate('AddItems')
+  }
 
-      let r = Math.floor(Math.random() * 7)
+  const handleGoToAddedItems = async () => {
+    let getAddedItems = await GetTasksByRoomId(myRoom.id)
+    if (getAddedItems.length > 0) {
+      let addedItemsWColor = [] as any
+      getAddedItems.map((task: any, idx: number) => addedItemsWColor.push({ ...task, 'color': (rState + idx + 2), 'spaceId': myRoom.id }))
 
-     const displayAddIcon = () => {
-        console.log('Plus Icon Works');
-        navigation.navigate('AddItems')
-      }
-
-      // const displayTasks = async() => {
-      //     let tasks = await GetTasksByRoomId(myRoom.id)
-      //     console.log(myRoom.id)
-      //     console.log(tasks)
-
-      //     if(tasks.length > 0) 
-      //     {
-      //       setRoomTasks(tasks)
-      //       console.log(tasks)
-      //     }
-      //     else{
-      //       setRoomTasks([])
-      //     }
-        
-         
-          
-
-     // }
-
-      const handleGoToAddedItems = async() => {
-        let getAddedItems = await GetTasksByRoomId(myRoom.id)
-        if(getAddedItems.length>0){
-          let addedItemsWColor = [] as any
-          getAddedItems.map((task:any, idx: number)=>  addedItemsWColor.push({ ...task, 'color': (rState + idx+2), 'spaceId': myRoom.id }))
-         
-          setStoredAddedItems(addedItemsWColor)
-          navigation.navigate('AddedItems')
-          setNoAddedItems(true)
-        }
-      }
+      setStoredAddedItems(addedItemsWColor)
+      navigation.navigate('AddedItems')
+      setNoAddedItems(true)
+    }
+  }
 
   return (
     <>
       <ScrollView>
-    <View style={styles.container}>
-         <HeaderComponent title="MY Added Tasks" />
-         <View style={{padding: 10}}>
-         <UnderlinedOneHeaderComponent titleFirst={"My Room: "+ myRoom.spaceName} />
-        </View>
-    </View>
-     <View style={styles.addExtraContainer}>
-     <AddItemButtonComponent onPress={displayAddIcon}>
-       <View style={{flexDirection: "row", alignItems: "center"}}>
-       <Entypo name="squared-plus" size={50} color={lilacColor} />
-       <UserNameComponent name="Add Items For more tasks"></UserNameComponent>
-       </View>
-     </AddItemButtonComponent>
-       
-   </View>
-
-    {/*//map all items here} */}
-    {
-      roomTasks.length > 0 ?
-      roomTasks.map((task:ITask, idx:number)=>{
-        return(
-
-          <View key={idx}>
-          <TaskRowFullInfoComponent r={r} key={idx} idx={idx} task={task} />
-          <View style={[ {display:current===task.id?"none": "none", padding: 10}]}>
-          <Text style={[styles.taskInfo ]}>Description: {task.task.description}</Text>
-           <Text style={[styles.taskInfo ]}>Item: {task.item.name}</Text>
-           <Text style={[styles.taskInfo ]}>Points: {task.task.coins} points</Text>
-           </View>
-
+        <View style={styles.container}>
+          <HeaderComponent title="MY Added Tasks" />
+          <View style={{ padding: 10 }}>
+            <UnderlinedOneHeaderComponent titleFirst={"My Room: " + myRoom.spaceName} />
           </View>
+        </View>
+        <View style={styles.addExtraContainer}>
+          <AddItemButtonComponent onPress={displayAddIcon}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Entypo name="squared-plus" size={50} color={lilacColor} />
+              <UserNameComponent name="Add Items For more tasks"></UserNameComponent>
+            </View>
+          </AddItemButtonComponent>
 
+        </View>
 
-
-          
-     
-        )
-      })
-      :
-      
-      <View style={[ {padding:10}]}>
-                    <UserNameComponent name="You have no tasks. Add items for a list of auto generated tasks."/>
+        {
+          roomTasks.length > 0 ?
+            roomTasks.map((task: ITask, idx: number) => {
+              return (
+                <View key={idx}>
+                  <TaskRowFullInfoComponent r={r} key={idx} idx={idx} task={task} />
+                  <View style={[{ display: current === task.id ? "none" : "none", padding: 10 }]}>
+                    <Text style={[styles.taskInfo]}>Description: {task.task.description}</Text>
+                    <Text style={[styles.taskInfo]}>Item: {task.item.name}</Text>
+                    <Text style={[styles.taskInfo]}>Points: {task.task.coins} points</Text>
+                  </View>
                 </View>
-    }
-    
-    
-    
-   </ScrollView>
-   {
-         roomTasks.length > 0 ?
-         <TwoFullButtonComponent color={purpleColor} text1={"Back"} text2={"Added Items"} onBackPress={()=>navigation.goBack()} onAcceptPress={handleGoToAddedItems} />
+              )
+            })
+            :
 
-         : 
+            <View style={[{ padding: 10 }]}>
+              <UserNameComponent name="You have no tasks. Add items for a list of auto generated tasks." />
+            </View>
+        }
 
+      </ScrollView>
+      {
+        roomTasks.length > 0 ?
+          <TwoFullButtonComponent color={purpleColor} text1={"Back"} text2={"Added Items"} onBackPress={() => navigation.goBack()} onAcceptPress={handleGoToAddedItems} />
 
-         <FullButtonComponent radius ={0} onPress={()=>navigation.goBack()} color={purpleColor}>
-      {/* AddSelectedTask(addTask), console.log(AddSelectedTask(addTask) */}
-        <Text>Back</Text>
-      </FullButtonComponent>
-   }
+          :
 
-</>
-
+          <FullButtonComponent radius={0} onPress={() => navigation.goBack()} color={purpleColor}>
+            <Text>Back</Text>
+          </FullButtonComponent>
+      }
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //alignItems: 'center',
-    justifyContent:"space-between",
-    
+    justifyContent: "space-between",
+
   },
-  addExtraContainer:{
+  addExtraContainer: {
     alignItems: 'center',
     flexDirection: 'row',
     margin: 10
   },
   taskInfo: {
     fontSize: 20,
-}
+  }
 });
 
 export default AddedTasksScreen

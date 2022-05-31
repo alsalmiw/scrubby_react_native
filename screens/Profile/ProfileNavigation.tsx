7b0785e562ec10698956ca7acf1e667ef3fc8bba
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 12:37:28 -0700

    format, ProfileNavigation

--- screens/Profile/ProfileNavigation.tsx
+++ screens/Profile/ProfileNavigation.tsx
@@ -1,9 +1,8 @@
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { FC } from 'react';
 import MyProfileScreen from '../Profile/MyProfileScreen'
 import AddedItemsScreen from '../Profile/AddedItemsScreen'
 import ChildTasksScreen from '../Profile/ChildTasksScreen';
-//import RootStackParamList from '../../types/INavigateProfile'
 import RootStackParamList from '../../types/INavigation'
 
 import AddNewSpaceScreen from '../Profile/AddNewSpaceScreen'
@@ -18,26 +17,26 @@ import EditProfileScreen from './EditProfileScreen';
 
 const Tab = createNativeStackNavigator<RootStackParamList>();
 
-const ProfileNavigation: FC = ()=> {
+const ProfileNavigation: FC = () => {
 
   return (
 
-        <Tab.Navigator initialRouteName="MyProfile">
-          <Tab.Screen name="MyProfile" component={MyProfileScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="AddNewSpace" component={AddNewSpaceScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="AddNewRoom" component={AddNewRoomScreen} options={{ headerShown: false }}/>
-           <Tab.Screen name="AddedItems" component={AddedItemsScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="ChildTasks" component={ChildTasksScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="AddItems" component={AddItemsScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="AddedTasks" component={AddedTasksScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="Rooms" component={SpaceRoomsScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="AddChild" component={AddChildScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="DefaultChildOptions" component={DefaultSpaceChildScreen} options={{ headerShown: false }}/>
-          <Tab.Screen name="EditProfile" component={EditProfileScreen} options={{ headerShown: false }}/>
+    <Tab.Navigator initialRouteName="MyProfile">
+      <Tab.Screen name="MyProfile" component={MyProfileScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="AddNewSpace" component={AddNewSpaceScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="AddNewRoom" component={AddNewRoomScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="AddedItems" component={AddedItemsScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="ChildTasks" component={ChildTasksScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="AddItems" component={AddItemsScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="AddedTasks" component={AddedTasksScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="Rooms" component={SpaceRoomsScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="AddChild" component={AddChildScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="DefaultChildOptions" component={DefaultSpaceChildScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="EditProfile" component={EditProfileScreen} options={{ headerShown: false }} />
 
 
 
-        </Tab.Navigator>
+    </Tab.Navigator>
   );
 }
 

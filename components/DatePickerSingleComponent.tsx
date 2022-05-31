76a3407a88403097486db221af80d14d34befa1c
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 10:30:26 -0700

    Deleted comments and format DatePickerSingleComp

--- components/DatePickerSingleComponent.tsx
+++ components/DatePickerSingleComponent.tsx
@@ -1,13 +1,5 @@
-// React Native Date Picker – To Pick the Date using Native Calendar
-// https://aboutreact.com/react-native-datepicker/
-
-// import React in our code
 import React, { useState } from 'react';
-
-// import all the components we are going to use
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-
-//import DatePicker from the package we installed
 import DatePicker from 'react-native-datepicker';
 
 const DatePickerSingleComponent = () => {
@@ -21,17 +13,16 @@ const DatePickerSingleComponent = () => {
         </Text>
         <DatePicker
           style={styles.datePickerStyle}
-          date={date} //initial date from state
-          mode="date" //The enum of date, datetime and time
+          date={date}
+          mode="date"
           placeholder="select date"
           format="DD-MM-YYYY"
-          minDate= ''
+          minDate=''
           maxDate="01-01-2019"
           confirmBtnText="Confirm"
           cancelBtnText="Cancel"
           customStyles={{
             dateIcon: {
-              //display: 'none',
               position: 'absolute',
               left: 0,
               top: 4,

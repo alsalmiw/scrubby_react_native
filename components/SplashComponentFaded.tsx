04cd65f74ccfe00781d69c0e95d5d5fba20a882f
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 10:46:40 -0700

    Formatted and spacing, SplashCompFaded

--- components/SplashComponentFaded.tsx
+++ components/SplashComponentFaded.tsx
@@ -6,28 +6,20 @@ import { ThemeContext } from "../context/ThemeContext"
 
 
 const SplashComponentFaded: FC<any> = ({ children }) => {
-    const {waiting, setWaiting} = useContext(UserContext)
+    const { waiting, setWaiting } = useContext(UserContext)
     const { fuchsiaColor, lilacColor, lightLilacColor, blueColor, purpleColor, greenColor } = useContext(ThemeContext);
-
-
-
 
     return (
         <>
-
             {waiting ?
                 <View style={styles.spinBackgroundColor}>
                     <View style={[styles.spinBackgroundColor, { justifyContent: 'center' }]}>
-
                         <SafeAreaView >
                             <ActivityIndicator color={blueColor} size="large" />
-                            <View style={{flexDirection:'row', justifyContent:'center'}}>
-                            <Text style={{color:blueColor}}>Loading, please wait...</Text>
+                            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                                <Text style={{ color: blueColor }}>Loading, please wait...</Text>
                             </View>
-                              
                         </SafeAreaView>
-                    
-
                     </View>
                 </View>
                 :
@@ -37,18 +29,15 @@ const SplashComponentFaded: FC<any> = ({ children }) => {
             }
         </>
 
-
-
-
     )
 }
 const styles = StyleSheet.create({
     spinBackgroundColor: {
 
-        //backgroundColor: 'white',
+        
         minWidth: "100%",
         minHeight: '100%',
-   
+
     },
 })
 

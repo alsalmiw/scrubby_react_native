baea4e1aafde8b3043229b4c3e2476f5ea011dcd
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 10:21:25 -0700

    Fixed console and space ChildFreeBoolComp

--- components/Settings/ChildFreeBoolComponent.tsx
+++ components/Settings/ChildFreeBoolComponent.tsx
@@ -2,50 +2,45 @@ import { useContext, useState, useEffect } from "react";
 import { StyleSheet, Switch, Text, View } from "react-native"
 import { ThemeContext } from "../../context/ThemeContext";
 import UserContext from "../../context/UserContext";
-import {ChildFreeSwitch} from  '../../services/dataService'
+import { ChildFreeSwitch } from '../../services/dataService'
 
-const ChildFreeBoolComponent = ()=>{
-    useEffect(()=>{
-
-
-    },[])
+const ChildFreeBoolComponent = () => {
+  useEffect(() => {
 
 
-    const {isChildFree, setIsChildFree,  userData} = useContext(UserContext)
-    const {violetColor, lilacColor} = useContext(ThemeContext)
+  }, [])
 
-    const toggleSwitch = async() => {
-      console.log(userData.id)
-      let switched:any = await ChildFreeSwitch(userData.id)
-      if(switched)
-      {
-        console.log("entered true")
-        setIsChildFree(!isChildFree);
-        console.log(isChildFree)
-      }
+
+  const { isChildFree, setIsChildFree, userData } = useContext(UserContext)
+  const { violetColor, lilacColor } = useContext(ThemeContext)
+
+  const toggleSwitch = async () => {
+    let switched: any = await ChildFreeSwitch(userData.id)
+    if (switched) {
+      setIsChildFree(!isChildFree);
     }
+  }
 
-    return (
-        <View style={styles.container}>
-          <Switch
-            //trackColor={{ false: "#767577", true: {lilacColor}}}
-            thumbColor={isChildFree ? "#f4f3f4" : "#f4f3f4"}
-            ios_backgroundColor={violetColor}
-            onValueChange={toggleSwitch}
-            value={isChildFree}
-           // {...setIsChildFree(isChildFree)}
-            style={{ transform: [{ scaleX: .7 }, { scaleY: .7 }] }}
-
-          />
-        </View>
-      );
+  return (
+    <View style={styles.container}>
+      <Switch
+        thumbColor={isChildFree ? "#f4f3f4" : "#f4f3f4"}
+        ios_backgroundColor={violetColor}
+        onValueChange={toggleSwitch}
+        value={isChildFree}
+        style={{ transform: [{ scaleX: .7 }, { scaleY: .7 }] }}
+      />
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "flex-end",
-      justifyContent: "center",
-    }
-  });
+
+  container: {
+    flex: 1,
+    alignItems: "flex-end",
+    justifyContent: "center",
+  }
+});
 
 export default ChildFreeBoolComponent

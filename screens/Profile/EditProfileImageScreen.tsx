d71085c42de9e4bfaadfa5b6a155eec7f31dc4f4
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 12:28:23 -0700

    Format doc, log, EditProfileImageScreen

--- screens/Profile/EditProfileImageScreen.tsx
+++ screens/Profile/EditProfileImageScreen.tsx
@@ -1,35 +1,34 @@
-// import { StatusBar } from 'expo-status-bar';
 import { FC, useContext, useState } from 'react';
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import {AddPhotoComponent} from '../../components/AddPhotoComponent';
+import { AddPhotoComponent } from '../../components/AddPhotoComponent';
 import RootStackParamList from '../../types/INavigation'
 import InputFieldComponent from '../../components/AddEdit/InputFieldComponent'
 import TitleComponent from '../../components/AddEdit/TitleComponent'
 import FullButtonComponent from '../../components/FullButtonComponent'
 import WhiteSubTitleComponent from '../../components/AddEdit/WhiteSubTitleComponent';
-import {UpdateName} from '../../services/dataService'
+import { UpdateName } from '../../services/dataService'
 import UserContext from '../../context/UserContext';
 import INewName from '../../Interfaces/INewName'
 import { ThemeContext } from '../../context/ThemeContext';
 
 
-type Props = NativeStackScreenProps <RootStackParamList, 'EditProfile'>
+type Props = NativeStackScreenProps<RootStackParamList, 'EditProfile'>
 
-const EditProfileScreen: FC<Props> = ({navigation, route})=> {
-  const {orangeColor, blueColor} = useContext(ThemeContext)
+const EditProfileScreen: FC<Props> = ({ navigation, route }) => {
+  const { orangeColor, blueColor } = useContext(ThemeContext)
   const [newName, setNewName] = useState('')
-  const {username} = useContext(UserContext)
+  const { username } = useContext(UserContext)
 
 
   const saveName = async () => {
-    let data:INewName = {
+    let data: INewName = {
       Username: username,
       Name: newName
     }
-    console.log(data)
+
     let result = await UpdateName(data)
-    if(result) {
+    if (result) {
       alert("You have successfully updated your name")
       navigation.navigate('SettingsScreen')
     }
@@ -38,22 +37,18 @@ const EditProfileScreen: FC<Props> = ({navigation, route})=> {
   const handleSave = () => {
     saveName()
   }
-  
+
   return (
- 
-    <View style={[styles.container,{backgroundColor:orangeColor}]}>
-        <TitleComponent title="Edit Profile" />
-        <AddPhotoComponent />
-        <WhiteSubTitleComponent title="Name" />
-        <InputFieldComponent maxLength={20} value={""} holder="enter your name" hide={false} onChangeText={(e: string)=>setNewName(e)} />
-        <FullButtonComponent radius={0} onPress={handleSave} color={blueColor}>
-          <Text>Save</Text>
-        </FullButtonComponent>
 
-        
+    <View style={[styles.container, { backgroundColor: orangeColor }]}>
+      <TitleComponent title="Edit Profile" />
+      <AddPhotoComponent />
+      <WhiteSubTitleComponent title="Name" />
+      <InputFieldComponent maxLength={20} value={""} holder="enter your name" hide={false} onChangeText={(e: string) => setNewName(e)} />
+      <FullButtonComponent radius={0} onPress={handleSave} color={blueColor}>
+        <Text>Save</Text>
+      </FullButtonComponent>
     </View>
-
-    
   );
 }
 

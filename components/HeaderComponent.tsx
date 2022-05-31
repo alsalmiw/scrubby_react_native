792415c77fddb814fe262d581ab887f234cb549d
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 10:34:45 -0700

    Formatted Document, HeaderComponent

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FC, useContext } from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
-import {ThemeContext} from "../context/ThemeContext"
+import { ThemeContext } from "../context/ThemeContext"
 
 interface TitleType {
     title: string;
@@ -18,19 +18,19 @@ const HeaderComponent: FC<TitleType> = ({ title }) => {
 }
 
 const styles = StyleSheet.create({
-        mainHeader: {
-            fontSize:25,
-            textTransform: "uppercase",
-            fontWeight: "bold",
-        }, 
-        headerContainer:{
-            paddingTop: 45,
-            paddingRight: 10,
-            width:'100%',
-            flexDirection: "row",
-            justifyContent: "flex-end",
-            marginBottom:30,
-        }
-    })
+    mainHeader: {
+        fontSize: 25,
+        textTransform: "uppercase",
+        fontWeight: "bold",
+    },
+    headerContainer: {
+        paddingTop: 45,
+        paddingRight: 10,
+        width: '100%',
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        marginBottom: 30,
+    }
+})
 
 export default HeaderComponent

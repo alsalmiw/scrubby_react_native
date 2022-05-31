d9c9e5112011b8bb6e5c6158dd27cf3659120656
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 11:23:03 -0700

    Format doc, IDefaultSpaceChild

--- Interfaces/IDefaultSpaceChild.tsx
+++ Interfaces/IDefaultSpaceChild.tsx
@@ -1,7 +1,7 @@
-export default interface IDefaultSpaceChild{
-    Id:number,
-    ChildId:number,
-    CollectionId:number,
-    IsDefault:boolean, 
-    IsDelete:boolean
+export default interface IDefaultSpaceChild {
+    Id: number,
+    ChildId: number,
+    CollectionId: number,
+    IsDefault: boolean,
+    IsDelete: boolean
 }

67236a72c805adc7b169c193e3c38027ff05cd8f
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 11:27:29 -0700

    format doc, IRoom

--- Interfaces/IRoom.tsx
+++ Interfaces/IRoom.tsx
@@ -1,6 +1,6 @@
 export default interface IRoom {
-    id:number, 
-    spaceName: string,
-    spaceCategory: string,
-    collectionId: number,
-  }
+  id: number,
+  spaceName: string,
+  spaceCategory: string,
+  collectionId: number,
+}

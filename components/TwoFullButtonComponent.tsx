2002aa7726ddb620f16018a07dd962856eeae16e
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 11:05:34 -0700

    format doc, TwoFullButtonComp

--- components/TwoFullButtonComponent.tsx
+++ components/TwoFullButtonComponent.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet, Pressable } from 'react-native';
 interface Comp {
     text1: string,
     text2: string,
-    color:string,
+    color: string,
     onBackPress: Function,
     onAcceptPress: Function,
 
@@ -20,22 +20,20 @@ const TwoFullButtonComponent: FC<Comp> = (props) => {
         props.onAcceptPress()
     }
     return (
-        <View style={[styles.buttonOuterContainer, {backgroundColor:props.color}]}>
-                <Pressable
-                    style={styles.buttonInnerContainer}
-                    onPress={backButtonHandler}
-                >
-                    <Text style={styles.buttonText}>{props.text1}</Text>
-                </Pressable>
-                <View style={styles.verticalLine}></View>
-                <Pressable
-                    style={styles.buttonInnerContainer}
-                    onPress={acceptButtonHandler}
-                >
-                    <Text style={styles.buttonText}>{props.text2}</Text>
-                </Pressable>
-                
-            
+        <View style={[styles.buttonOuterContainer, { backgroundColor: props.color }]}>
+            <Pressable
+                style={styles.buttonInnerContainer}
+                onPress={backButtonHandler}
+            >
+                <Text style={styles.buttonText}>{props.text1}</Text>
+            </Pressable>
+            <View style={styles.verticalLine}></View>
+            <Pressable
+                style={styles.buttonInnerContainer}
+                onPress={acceptButtonHandler}
+            >
+                <Text style={styles.buttonText}>{props.text2}</Text>
+            </Pressable>
         </View>
     )
 };

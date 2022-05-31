74cadb7685411a9f081e8cf1a5e45df81d92169b
Daniel Bautista <dbautista9015@gmail.com>
2022-05-31 11:06:41 -0700

    format doc, UnderlinedHeaderComponent

--- components/UnderlinedHeaderComponent.tsx
+++ components/UnderlinedHeaderComponent.tsx
@@ -1,30 +1,30 @@
 import React from "react";
 import { FC, useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import {ThemeContext} from "../context/ThemeContext"
+import { ThemeContext } from "../context/ThemeContext"
 import UserContext from "../context/UserContext";
 
-interface TitleType{
+interface TitleType {
     titleOne: string;
     titleTwo: any;
     titleThree: any;
 
 }
 
-const UnderlinedHeaderComponent: FC<TitleType> =({titleOne, titleTwo, titleThree}) => {
-    const {secondaryTextColor, lightLilacColor} = useContext(ThemeContext)
-    const {seeAll, setSeeAll} = useContext(UserContext)
+const UnderlinedHeaderComponent: FC<TitleType> = ({ titleOne, titleTwo, titleThree }) => {
+    const { secondaryTextColor, lightLilacColor } = useContext(ThemeContext)
+    const { seeAll, setSeeAll } = useContext(UserContext)
 
 
-    return(
-        <View style={[styles.headerContainer, {borderBottomColor: lightLilacColor} ]}>
-        <Text style={[styles.mainHeader, {color:secondaryTextColor}]}>{titleOne}</Text>
-        {
-            seeAll?
-                    <Text style={[styles.mainHeader, {color:secondaryTextColor}, {textDecorationLine:'underline'}]} onPress={()=> setSeeAll(!seeAll)}>{titleTwo}</Text>
+    return (
+        <View style={[styles.headerContainer, { borderBottomColor: lightLilacColor }]}>
+            <Text style={[styles.mainHeader, { color: secondaryTextColor }]}>{titleOne}</Text>
+            {
+                seeAll ?
+                    <Text style={[styles.mainHeader, { color: secondaryTextColor }, { textDecorationLine: 'underline' }]} onPress={() => setSeeAll(!seeAll)}>{titleTwo}</Text>
                     :
-                    <Text style={[styles.mainHeader, {color:secondaryTextColor}, {textDecorationLine:'underline'}]}  onPress={()=> setSeeAll(!seeAll)}>{titleThree}</Text>
-        }
+                    <Text style={[styles.mainHeader, { color: secondaryTextColor }, { textDecorationLine: 'underline' }]} onPress={() => setSeeAll(!seeAll)}>{titleThree}</Text>
+            }
 
         </View>
     )
@@ -32,19 +32,18 @@ const UnderlinedHeaderComponent: FC<TitleType> =({titleOne, titleTwo, titleThree
 
 
 const styles = StyleSheet.create({
-        mainHeader: {
-            
-            fontSize:20,
-            fontWeight: "bold", 
-        }, 
-        headerContainer:{
-            flex:1,
-            width:'95%',
-            flexDirection: "row",
-            justifyContent: "space-between",
-           
-            borderBottomWidth:1,
-        }
-    })
+    mainHeader: {
+
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    headerContainer: {
+        flex: 1,
+        width: '95%',
+        flexDirection: "row",
+        justifyContent: "space-between",
+        borderBottomWidth: 1,
+    }
+})
 
 export default UnderlinedHeaderComponent
